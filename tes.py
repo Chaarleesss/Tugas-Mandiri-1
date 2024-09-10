1b7e43c4acb25703fffb77e919e8b89e9b132e66
Charles Andriansyah <charlesandriansyah104@gmail.com>
2024-09-10 11:54:47 +0700

    first commit

---
+++ tes.py
@@ -0,0 +1,41 @@
+# 1.Penjumlahan
+var1 = int(input("Masukkan bilangan penjumlahan pertama: "))
+var2 = int(input("Masukkan bilangan penjumlahan kedua: "))
+penjumlahan = var1 + var2
+print("Hasil penjumlahan:", penjumlahan)
+
+# 2.Pengurangan
+Pengurangan1 = int(input("Masukkan bilangan pengurangan pertama: "))
+Pengurangan2 = int(input("Masukkan bilangan pengurangan kedua: "))
+pengurangan = Pengurangan1 - Pengurangan2
+print("Hasil Pengurangan:", pengurangan)
+
+# 3.Perkalian
+Perkalian1 = int(input("Masukkan bilangan perkalian pertama: "))
+Perkalian2 = int(input("Masukkan bilangan perkalian kedua: "))
+perkalian = Perkalian1 * Perkalian2
+print("Hasil Perkalian:", perkalian)
+
+# 4.Perpangkatan
+Perpangkatan1 = int(input("Masukkan bilangan perpangkatan pertama: "))
+Perpangkatan = int(input("Masukkan bilangan perpangkatan kedua: "))
+perpangkatan = Perpangkatan1 ** Perpangkatan
+print("Hasil perpangkatan", perpangkatan)
+
+# 5.Integer Division
+Interdiv1 = int(input("Masukkan bilangan InterDiv pertama: "))
+Interdiv2 = int(input("Masukkan bilangan InterDiv kedua: "))
+InterDiv = Interdiv1 // Interdiv2
+print("Hasil Integer Division:", InterDiv)
+
+# 6.Float Division
+Float1 = int(input("Masukkan bilangan FloatDiv pertama: "))
+Float2 = int(input("Masukkan bilangan FloatDiv kedua: "))
+FloatDiv = Float1 / Float2
+print("Hasil Float Division:", FloatDiv)
+
+# 7.Modulus
+Modul1 = int(input("Masukkan bilangan Modulo pertama: "))
+Modul2 = int(input("Masukkan bilangan Modulo kedua: "))
+Modulo = Modul1 % Modul2
+print("Hasil Modulus Division:", Modulo)
