aec32cd4d595f4b51d8c1d3ef5e673bf79359373
Charles Andriansyah <charlesandriansyah104@gmail.com>
2024-09-20 11:11:52 +0700

    finish

--- praktikum1.py
+++ praktikum1.py
@@ -1,15 +1,11 @@
-nama1 = input("Nama kamu siapa? ")
-asal1 = input("Darimana asal kamu? ")
-sekolah1 = input("Sebelumnya bersekolah dimana? ")
+nama = input("Nama kamu siapa? ")
+asal = input("Darimana asal kamu? ")
+sekolah = input("Sebelumnya bersekolah dimana? ")
 
 
-# print("nama kamu siapa?", nama1)
-# print("darimana asal kamu", nama1,"? ", asal1)
-# print("sebelumnya bersekolah dimana? ", sekolah1)
 
-
-print("Hallo selamat siang ", nama1)
-print("Wah dari", asal1)
-print("Pasti menyenangkan di", sekolah1)
+print("Hallo selamat siang ", nama)
+print("Wah dari", asal)
+print("Pasti menyenangkan di", sekolah)
 
 
