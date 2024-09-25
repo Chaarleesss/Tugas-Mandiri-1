a62c14aa15a771740a8573132a647b24bc71cb4c
Charles Andriansyah <charlesandriansyah104@gmail.com>
2024-09-25 22:12:59 +0700

    lower case to satuan

--- praktikum2.py
+++ praktikum2.py
@@ -2,13 +2,15 @@ panjang = int(input("Masukan Panjang Ruangan : "))
 lebar = int(input("Masukan Lebar Ruangan :  "))
 satuan = input("Masukan Satuan (Meter/Inci) : ")
 
+
 meter1 = panjang*lebar
 inci1 = panjang+lebar
+satuan1 = satuan.lower()
 
-if satuan == "meter" : 
+if satuan1 == "meter" : 
     print("Luas ruangan dengan",panjang ,"dan lebar", lebar, "adalah", meter1)
-elif satuan == "Meter" :
-        print("Luas ruangan dengan",panjang ,"dan lebar", lebar, "adalah", meter1)
+# elif satuan1 == "Meter" :
+#         print("Luas ruangan dengan",panjang ,"dan lebar", lebar, "adalah", meter1)
 else :
     print("inci ruangan dengan",panjang ,"dan lebar", lebar, "adalah", inci1)
 
