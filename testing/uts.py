0c776afddad69304ea73b81096adb80445e7bbd3
Charles Andriansyah <charlesandriansyah104@gmail.com>
2024-10-14 10:40:38 +0700

    call name

--- testing/uts.py
+++ testing/uts.py
@@ -1,3 +1,4 @@
+nama = input("Masukan Nama anda : " )
 nilai_Tugas = int(input("Masukan Nilai Tugas Anda : "))
 nilai_UTS = int(input("Masukan Nilai UTS Anda : "))
 nilai_UAS = int(input("Masukan Nilai UAS Anda : "))
@@ -7,4 +8,4 @@ niilai_Akhir = (0.3 * nilai_Tugas + 0.3 * nilai_UTS + 0.4 * nilai_UAS)
 if(niilai_Akhir >  100) :
     print("nilai anda tidak valid")
 else :
-    print(f"Nilai Akhir alan adalah : {niilai_Akhir}")
+    print(f"Nilai Akhir {nama} adalah : {niilai_Akhir}")
