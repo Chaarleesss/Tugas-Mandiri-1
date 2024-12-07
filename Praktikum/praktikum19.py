757c2af8c5937f68548aeece90ccca0c860e3eba
Charles Andriansyah <charlesandriansyah104@gmail.com>
2024-12-07 19:31:44 +0700

    recursive concept

--- Praktikum/praktikum19.py
+++ Praktikum/praktikum19.py
@@ -1,9 +1,17 @@
-def main() : 
-    arr = [15, 2, 208, 12, 5, 75, 200, 100, 1]
-    print(f'List sebelum di sorting : \n', arr)
+def bubble_sort_recursive(arr, n=None):
+    if n is None:
+        n = len(arr)
     
-    arr.sort()
-    print(f'List sesudah di sorting : \n', arr)
+    if n == 1:
+        return arr
+    
+    for i in range(n - 1):
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    
+    return bubble_sort_recursive(arr, n - 1)
 
-
-main()
+arr = [87, 56, 34, 23, 89, 15, 2, 200, 28, 31]
+print("List Sebelum di sorting : \n", arr)
+sorted_arr = bubble_sort_recursive(arr)
+print("List Sesudah di sorting : \n", sorted_arr)
