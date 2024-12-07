3477ec79df14a0389f38889ef6598e72c68c8e6a
Charles Andriansyah <charlesandriansyah104@gmail.com>
2024-12-07 10:03:34 +0700

    sorting

--- Praktikum/praktikum19.py
+++ Praktikum/praktikum19.py
@@ -1,31 +1,9 @@
-class Mahasiswa:
-    def __init__(self, nama, nim, jurusan, angkatan,):
-        self.nama = nama
-        self.nim = nim
-        self.jurusan = jurusan
-        self.angkatan = angkatan
-
-    def tampilkan_biodata(self):
-        print("\n=== Biodata Mahasiswa ===")
-        print(f"Nama    : {self.nama}")
-        print(f"NIM     : {self.nim}")
-        print(f"Jurusan : {self.jurusan}")
-        print(f"angkatan  : {self.angkatan}")
-
-
-def main():
-    total_mahasiswa = 0
-    nama = input("Masukkan nama: ")
-    nim = input("Masukkan NIM: ")
-    jurusan = input("Masukkan jurusan: ")
-    angkatan = input("Masukkan angkatan: ")
-    total_mahasiswa += 1
-
-    mahasiswa = Mahasiswa(nama, nim, jurusan, angkatan)
-
-    mahasiswa.tampilkan_biodata()
-    print(f"\nTotal mahasiswa yang telah diinput: {total_mahasiswa}")
-
+def main() : 
+    arr = [15, 2, 208, 12, 5, 75, 200, 100, 1]
+    print(f'List sebelum di sorting : \n', arr)
+    
+    arr.sort()
+    print(f'List sesudah di sorting : \n', arr)
 
 
 main()
